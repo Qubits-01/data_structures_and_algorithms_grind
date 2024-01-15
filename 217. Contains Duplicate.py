360341d11610cea89f8211e298250bcc71d89f81
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-01-15 11:03:54 +0800

    Impr: 217. Contains Duplicate (Accepted)

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -5,15 +5,12 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # Know if there is a duplicate in the list
-        # 1. Sort the list
-        # 2. Compare the current element with the next element
-        # 3. If they are equal, return True
-        # 4. If the loop ends, return False
+        hashSet = set()
 
-        nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        for n in nums:
+            if n in hashSet:
                 return True
+
+            hashSet.add(n)
 
         return False
