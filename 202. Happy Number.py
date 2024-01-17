790f9c4d31185921f44bee5f4b129a7035e021a1
Marc Peejay Viernes <mpvviernes@gmail.com>
2024-01-17 11:05:43 +0800

    202. Happy Number (Pass)

---
+++ 202. Happy Number.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/happy-number/
+
+
+from typing import List
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        # Create a set to store the previous numbers.
+        prevSet = set()
+
+        # Loop until the number is 1 or the number is in the set.
+        while n != 1 and n not in prevSet:
+            # Add the current number to the set.
+            prevSet.add(n)
+
+            # Get the sum of the squares of the digits.
+            n = sum([int(d) ** 2 for d in str(n)])
+
+        # Return whether the number is 1.
+        return n == 1
